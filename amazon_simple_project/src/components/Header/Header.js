690a71586a8649d553b1f8cc1d498c204ee3adfe
progmamun <mamun333089@gmail.com>
2021-07-30 23:07:55 +0600

    header style add

--- amazon_simple_project/src/components/Header/Header.js
+++ amazon_simple_project/src/components/Header/Header.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import logo from '../../images/logo.png';
+import './Header.css';
 
 const Header = () => {
   return (
-    <div>
+    <div className="header">
       <img src={logo} alt="" />
     </div>
   );
