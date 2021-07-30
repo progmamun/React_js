d722dd1f73654042c3ac70a91930cb1bd5ec367f
progmamun <mamun333089@gmail.com>
2021-07-30 23:28:00 +0600

    header done

--- amazon_simple_project/src/components/Header/Header.js
+++ amazon_simple_project/src/components/Header/Header.js
@@ -6,6 +6,11 @@ const Header = () => {
   return (
     <div className="header">
       <img src={logo} alt="" />
+      <nav>
+        <a href="/shop">shop</a>
+      <a href="/review">Order review</a>
+      <a href="/manage">Manage inventory</a>
+      </nav>
     </div>
   );
 };
