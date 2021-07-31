b17a880ce4cf84dfc20b978049fde3b13da983e8
progmamun <mamun333089@gmail.com>
2021-07-31 11:42:09 +0600

    shop page style

--- amazon_simple_project/src/components/Shop/Shop.js
+++ amazon_simple_project/src/components/Shop/Shop.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import fakeData from '../../fakeData';
 import { useState } from 'react';
+import './Shop.css';
 
 
 const Shop = () => {
@@ -8,14 +9,17 @@ const Shop = () => {
   const [products, setProducts] = useState(first10);
 
   return (
-    <div>
-      <h1>This is shop</h1>
-      <h3>{products.length}</h3>
-      <ul>
-        {
-          products.map(product => <li>{product.name}</li>)
-        }
-      </ul>
+    <div className="shop-container">
+      <div className="product-container">
+        <ul>
+          {
+            products.map(product => <li>{product.name}</li>)
+          }
+        </ul>
+      </div>
+      <div className="cart-container">
+        <h2>This is cart</h2>
+      </div>
     </div>
   );
 };
