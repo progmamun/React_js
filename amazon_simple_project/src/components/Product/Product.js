9da44061e8b728cb4dcd27f611184b8c95892aba
progmamun <mamun333089@gmail.com>
2021-07-31 16:52:25 +0600

    product style

--- amazon_simple_project/src/components/Product/Product.js
+++ amazon_simple_project/src/components/Product/Product.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import './Product.css';
 
 const Product = (props) => {
+  const {img, name, seller, price, stock} = props.product;
   return (
-    <div>
-      <h4>{props.product.name}</h4>
+    <div className="product">
+      <div>
+        <img src={img} alt=""></img>
+      </div>
+      <div>
+      <h4 className="product-name">{name}</h4>
+      <br />
+      <p><small>by: {seller}</small></p>
+      <p>${price}</p>
+      <br />
+      <p><small>Only {stock} left in stock - Order soon</small></p>
+      </div>
     </div>
   );
 };
